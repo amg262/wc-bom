4060dcda29948faac903600b1fb8ff77d2e99725
Andrew <andrewmgunn26@gmail.com>
2017-03-06 23:17:08 -0600

    all

--- includes/helper_scripts.php
+++ includes/helper_scripts.php
@@ -98,6 +98,155 @@ function my_option_group_validate($input) {
 
 }
 
+/**
+ * Copy image title and save to Alt text field when image is uploaded. Runs anytime
+ * an image is uploaded, automatically.
+ */
+
+add_filter( 'add_attachment', 'insert_image_alt_tag', 10, 2 );
+
+//add_filter('edit_attachment', 'insert_image_alt_tag', 10, 2);
+
+function insert_image_alt_tag( $post_ID ) {
+
+	$sit_settings = get_option( 'sit_settings' );
+
+	$attach = wp_get_attachment_url( $post_ID );
+
+	$title = sanitize_text_field( get_the_title( $post_ID ) );
+
+	if ( ! add_post_meta( $post_ID, '_wp_attachment_image_alt', $title, true ) ) {
+
+		update_post_meta( $post_ID, '_wp_attachment_image_alt', $title );
+
+	}
+
+}
+
+function batch_update_image_tags( $is_update ) {
+
+	$total = 0;
+
+	$created = 0;
+
+	$updated = 0;
+
+	$deleted = 0;
+
+	$args = [
+
+		'post_type' => 'attachment',
+
+		'numberposts' => - 1,
+
+		'post_status' => null,
+
+		'post_parent' => null, // any parent
+
+	];
+
+	//Get all attachment posts
+
+	$attachments = get_posts( $args );
+
+	//if there are posts
+
+	if ( $attachments ) {
+
+		$sit_settings = get_option( 'sit_settings' );
+
+		if ( $sit_settings[ 'enable_pdf' ] ) {
+
+			$pdf = true;
+
+			$pdf_mine = 'pdf';
+
+		}
+
+		$image_mime = 'image';
+
+		//Loop thru each attachment
+
+		foreach ( $attachments as $post ) {
+
+			//get post data ready,set title var to post title
+
+			setup_postdata( $post );
+
+			$title = sanitize_text_field( get_the_title( $post->ID ) );
+
+			$type = get_post_mime_type( $post->ID );
+
+			$tag = sanitize_text_field( get_post_meta( $post->ID, '_wp_attachment_image_alt', true ) );
+
+			$tag_str = strval( $tag );
+
+			$tag_len = strlen( $tag_str );
+
+			//echo $type;
+
+			if ( strpos( $type, $image_mime ) !== false ) {
+
+				if ( $is_update == true ) {
+
+					//if has post meta for alt tag, update it else add it.
+
+					if ( ! add_post_meta( $post->ID, '_wp_attachment_image_alt', $title, true ) ) {
+
+						if ( ( empty( $tag ) || ( ( $tag_len <= 2 ) && ( $tag_str !== $title ) ) ) ) {
+
+							update_post_meta( $post->ID, '_wp_attachment_image_alt', $title );
+
+							$updated ++;
+
+						}
+
+					} else {
+
+						$created ++; //update counter
+
+					}
+
+				} else {
+
+					//if has post meta for alt tag, update it else add it.
+
+					if ( ! empty( $tag ) ) {
+
+						delete_post_meta( $post->ID, '_wp_attachment_image_alt', $title );
+
+						$deleted ++; //update counter
+
+					} //end add_post_meta
+
+				}
+
+				$total ++;
+
+			}
+
+		} //end foreach
+
+	} //end attachments
+
+	$count = [
+
+		'total' => $total,
+
+		'created' => $created,
+
+		'updated' => $updated,
+
+		'deleted' => $deleted,
+
+	];
+
+	//count of files updated
+
+	return $count;
+
+}
+
 class Pugs_API_Endpoint{
 
 	/**
