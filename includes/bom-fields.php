36f0d2c854c903d941c8f4386a90fb223645036a
Andrew <andrewmgunn26@gmail.com>
2017-05-01 07:57:09 -0500

    nnom

--- includes/bom-fields.php
+++ includes/bom-fields.php
@@ -98,12 +98,12 @@ if( function_exists('acf_add_local_field_group') ):
 					'id' => '',
 				),
 				'taxonomy' => 'assembly_category',
-				'field_type' => 'checkbox',
 				'allow_null' => 0,
 				'add_term' => 1,
 				'save_terms' => 1,
 				'load_terms' => 1,
 				'return_format' => 'object',
+				'field_type' => 'checkbox',
 				'multiple' => 0,
 			),
 			array (
@@ -174,8 +174,8 @@ if( function_exists('acf_add_local_field_group') ):
 							'id' => '',
 						),
 						'post_type' => array (
-							0 => 'assembly',
-							1 => 'part',
+							0 => 'part',
+							1 => 'assembly',
 						),
 						'taxonomy' => array (
 						),
@@ -546,6 +546,93 @@ if( function_exists('acf_add_local_field_group') ):
 			1 => 'custom_fields',
 			2 => 'author',
 		),
+		'active' => 1,
+		'description' => '',
+	));
+
+	acf_add_local_field_group(array (
+		'key' => 'group_59072f06c3518',
+		'title' => 'Product',
+		'fields' => array (
+			array (
+				'key' => 'field_59072f355461d',
+				'label' => 'Materials',
+				'name' => 'materials',
+				'type' => 'repeater',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array (
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'collapsed' => '',
+				'min' => 0,
+				'max' => 0,
+				'layout' => 'table',
+				'button_label' => '',
+				'sub_fields' => array (
+					array (
+						'key' => 'field_59072f615461e',
+						'label' => 'Assembly',
+						'name' => 'assembly',
+						'type' => 'post_object',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array (
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'post_type' => array (
+							0 => 'assembly',
+						),
+						'taxonomy' => array (
+						),
+						'allow_null' => 0,
+						'multiple' => 0,
+						'return_format' => 'object',
+						'ui' => 1,
+					),
+					array (
+						'key' => 'field_59072fc85461f',
+						'label' => 'Qty',
+						'name' => 'qty',
+						'type' => 'text',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array (
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'default_value' => '',
+						'placeholder' => '',
+						'prepend' => '',
+						'append' => '',
+						'maxlength' => '',
+					),
+				),
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'post_type',
+					'operator' => '==',
+					'value' => 'product',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
 		'active' => 1,
 		'description' => '',
 	));
