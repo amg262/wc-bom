c44e193e963c2c9602991c367645e0a97f1699b5
Andrew <andrewmgunn26@gmail.com>
2017-05-12 05:53:28 -0500

    hf

--- classes/class-wc-bom-logger.php
+++ classes/class-wc-bom-logger.php
@@ -9,67 +9,59 @@
 namespace WooBom;
 
 
-use function file_put_contents;
+use function fclose;
+use const FILE_APPEND;
+use function file_exists;
+use function file_get_contents;
+use function fseek;
+use const SEEK_CUR;
+use function trailingslashit;
 
 class WC_Bom_Logger {
 
 	private $file, $data, $log, $path;
-	private $zipped, $unzipped;
-
+	private $filename, $filepath, $filedir, $unzipped;
 
 
 	public function __construct() {
 		add_action( 'admin_init', [ $this, 'init' ] );
 
-		$file = $wroot->check_file('.tmp/wr.txt');
-		$zipp = $wroot->unzip($file);
-		$wroot->save_file('.tmp/wr.php', $zipp);
 
-		$file = $wroot->check_file('.tmp/wu.txt');
-		$unzz = $wroot->unzip($file);
-		$wroot->save_file('.tmp/wu.php', $unzz);
 	}
 
 	public function init() {
 		//$this->logger_write();
-		$this->write_log( 'cache.txt', 'yeahhhheayhhh yeahhhfahe' );
+		$this->save_file( 'wcbom.txt', " SSSBOOBS"  );
+	}
+
+	/**
+	 * @param $filename
+	 * @param $data
+	 *
+	 * @return bool|int|void
+	 */
+	public function save_file( $filename, $data ) {
+
+		$this->filename = $filename;
+		$this->filedir  = WC_BOM_LOGS;
+		$flags          = [ FILE_APPEND ];
+
+		$this->filepath = trailingslashit( $this->filedir ) . $this->filename;
+		$n              = '/n';
+
+
+		$f = fopen(WC_BOM_LOGS.$filename, 'a+');
+		//fseek($f, SEEK_END);
+		fwrite($f, '/n');
+		fwrite($f, $data);
+		fclose($f);
 
 	}
 
-	public function save_file($filename, $data, $new = false) {
+	public function get_file( $filename ) {
+		$this->file = file_get_contents( $filename );
 
-		if ($new  )
-		$this->file = file_put_contents($file, $data);
-	}
-
-	public function check_file($file) {
-		$this->file = file_get_contents($file);
 		return $this->file;
-	}
-
-	public function write_log( $filename, $data, $new = false ) {
-
-		$flags = [ FILE_APPEND, LOCK_EX ];
-		$path  = WC_BOM_LOGS .'/'. date( 'dmy' ) . '_' . $filename;
-		$text  = $data;
-
-		if ( $new === true ) {
-			$flags = [ LOCK_EX ];
-		}
-
-		return file_put_contents( $path, $text, $flags );
-	}
-
-	public function return_log( $filename ) {
-		return file_get_contents( WC_BOM_LOGS . $filename );
-	}
-
-	public function delete_log( $file_list ) {
-		$list = (array) $file_list;
-
-		foreach ( $list as $obj ) {
-			unlink( $obj );
-		}
 	}
 
 	public function encrypt( $data ) {
@@ -90,5 +82,4 @@ class WC_Bom_Logger {
 }
 
 $log = new WC_Bom_Logger();
-$log->write_log( 'cache.txt', 'yeahhhheayhhh yeahhhfahe', true );
 
