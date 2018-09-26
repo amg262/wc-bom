1f9d7ccc72c4e9da3f39970dec049cb85822b7ef
Andrew Gunn <andrewmgunn26@gmail.com>
2018-09-26 03:51:46 -0500

    gg

--- wc-bom.php
+++ wc-bom.php
@@ -23,22 +23,23 @@
 
 //namespace WC_Bom;
 
-if ( ! defined( 'ABSPATH' ) ) {
-	die( 'No direct access allowed' );
+if (! defined('ABSPATH')) {
+    die('No direct access allowed');
 }
 
-function check_woocommerce() {
-	if ( class_exists( 'Woocommerce' ) ) {
-		$text = '<div class="error"><p><strong>WooBOM</strong> needs WooCommerce installed and activated to work!</p></div> ';
-		echo $text;
-	}
+function check_woocommerce()
+{
+    if (class_exists('Woocommerce')) {
+        $text = '<div class="error"><p><strong>WooBOM</strong> needs WooCommerce installed and activated to work!</p></div> ';
+        echo $text;
+    }
 }
 
-add_action( 'admin_notices', 'check_woocommerce' );
+add_action('admin_notices', 'check_woocommerce');
 //
 ///* Checks to see if the acf pro plugin is activated  */
 //if ( ! is_plugin_active( 'advanced-custom-fields-pro/acf.php' ) ) {
-//	/* load the plugin and anything else you want to do */
+//  /* load the plugin and anything else you want to do */
 //}
 //
 ///* Checks to see if the acf plugin is activated  */
@@ -46,187 +47,199 @@ add_action( 'admin_notices', 'check_woocommerce' );
 //
 //}
 /** Start: Detect ACF Pro plugin. Include if not present. */
-if ( ! class_exists( 'acf' ) ) { // if ACF Pro plugin does not currently exist
-	/** Start: Customize ACF path */
-	add_filter( 'acf/settings/path', 'cysp_acf_settings_path' );
-	function cysp_acf_settings_path( $path ) {
+if (! class_exists('acf')) { // if ACF Pro plugin does not currently exist
+    /** Start: Customize ACF path */
+    add_filter('acf/settings/path', 'cysp_acf_settings_path');
+    function cysp_acf_settings_path($path)
+    {
 
-		$path = plugin_dir_path( __FILE__ ) . 'acf/';
+        $path = plugin_dir_path(__FILE__) . 'acf/';
 
-		return $path;
-	}
+        return $path;
+    }
 
-	/** End: Customize ACF path */
-	/** Start: Customize ACF dir */
-	add_filter( 'acf/settings/dir', 'cysp_acf_settings_dir' );
-	function cysp_acf_settings_dir( $path ) {
+    /** End: Customize ACF path */
+    /** Start: Customize ACF dir */
+    add_filter('acf/settings/dir', 'cysp_acf_settings_dir');
+    function cysp_acf_settings_dir($path)
+    {
 
-		$dir = plugin_dir_url( __FILE__ ) . 'acf/';
+        $dir = plugin_dir_url(__FILE__) . 'acf/';
 
-		return $dir;
-	}
+        return $dir;
+    }
 
-	/** End: Customize ACF path */
-	/** Start: Hide ACF field group menu item */
-	//	add_filter( 'acf/settings/show_admin', '__return_false' );
-	/** End: Hide ACF field group menu item */
-	/** Start: Include ACF */
-	include_once( plugin_dir_path( __FILE__ ) . 'acf/acf.php' );
-	/** End: Include ACF */
-	/** Start: Create JSON save point */
-	add_filter( 'acf/settings/save_json', 'cysp_acf_json_save_point' );
-	function cysp_acf_json_save_point( $path ) {
+    /** End: Customize ACF path */
+    /** Start: Hide ACF field group menu item */
+    //  add_filter( 'acf/settings/show_admin', '__return_false' );
+    /** End: Hide ACF field group menu item */
+    /** Start: Include ACF */
+    include_once(plugin_dir_path(__FILE__) . 'acf/acf.php');
+    /** End: Include ACF */
+    /** Start: Create JSON save point */
+    add_filter('acf/settings/save_json', 'cysp_acf_json_save_point');
+    function cysp_acf_json_save_point($path)
+    {
 
-		$path = plugin_dir_path( __FILE__ ) . 'acf-json/';
+        $path = plugin_dir_path(__FILE__) . 'acf-json/';
 
-		return $path;
-	}
+        return $path;
+    }
 
-	/** End: Create JSON save point */
-	/** Start: Create JSON load point */
-	add_filter( 'acf/settings/load_json', 'cysp_acf_json_load_point' );
-	/** End: Create JSON load point */
-	/** Start: Stop ACF upgrade notifications */
-	add_filter( 'site_transient_update_plugins', 'cysp_stop_acf_update_notifications', 11 );
-	function cysp_stop_acf_update_notifications( $value ) {
+    /** End: Create JSON save point */
+    /** Start: Create JSON load point */
+    add_filter('acf/settings/load_json', 'cysp_acf_json_load_point');
+    /** End: Create JSON load point */
+    /** Start: Stop ACF upgrade notifications */
+    add_filter('site_transient_update_plugins', 'cysp_stop_acf_update_notifications', 11);
+    function cysp_stop_acf_update_notifications($value)
+    {
 
-		unset( $value->response[ plugin_dir_path( __FILE__ ) . 'acf/acf.php' ] );
+        unset($value->response[ plugin_dir_path(__FILE__) . 'acf/acf.php' ]);
 
-		return $value;
-	}
-	/** End: Stop ACF upgrade notifications */
+        return $value;
+    }
+    /** End: Stop ACF upgrade notifications */
 } else { // else ACF Pro plugin does exist
-	/** Start: Create JSON load point */
-	add_filter( 'acf/settings/load_json', 'cysp_acf_json_load_point' );
-	/** End: Create JSON load point */
+    /** Start: Create JSON load point */
+    add_filter('acf/settings/load_json', 'cysp_acf_json_load_point');
+    /** End: Create JSON load point */
 } // end-if ACF Pro plugin does not currently exist
 /** End: Detect ACF Pro plugin. Include if not present. */
 /** Start: Function to create JSON load point */
-function cysp_acf_json_load_point( $paths ) {
+function cysp_acf_json_load_point($paths)
+{
 
-	$paths[] = plugin_dir_path( __FILE__ ) . 'acf-json-load';
+    $paths[] = plugin_dir_path(__FILE__) . 'acf-json-load';
 
-	return $paths;
+    return $paths;
 }
 
-class WCB_Core {
+class WCB_Core
+{
 
-	/**
-	 * @var null
-	 */
-	protected static $instance = null;
-
-
-	/**
-	 * WCB_Init constructor.
-	 */
-	private function __construct() {
-
-		$this->init();
-
-	}
-
-	/**
-	 *
-	 */
-	protected function init() {
-		require __DIR__ . '/includes/class-wcb-post.php';
-		require __DIR__ . '/includes/class-wcb-groups.php';
-		require __DIR__ . '/includes/admin/class-wcb-settings-api.php';
-		require __DIR__ . '/includes/admin/class-wcb-settings.php';
-
-		$g = WCB_Field_Groups::getInstance();
-		$t = new WeDevs_Settings_API_Test();
-		$p = WCB_Post::getInstance();
-
-		add_action( 'init', [ $this, 'load_assets' ] );
-
-		//add_action( 'init', [ $this, 'check_acf' ] );
-		add_filter( 'plugin_action_links', [ $this, 'plugin_links' ], 10, 5 );
-		register_activation_hook( __FILE__, [ $this, 'activate' ] );
-		register_deactivation_hook( __FILE__, [ $this, 'deactivate' ] );
-	}
-
-	/**
-	 * @return null
-	 */
-	public static function getInstance() {
-
-		if ( static::$instance === null ) {
-			static::$instance = new static;
-		}
-
-		return static::$instance;
-	}
-
-	/**
-	 *
-	 */
-	public function activate() {
-		flush_rewrite_rules();
-	}
-
-	/**
-	 *
-	 */
-	public function deactivate() {
-		flush_rewrite_rules();
-	}
-
-	/**
-	 *
-	 */
-	public function load_assets() {
-
-		wp_register_script( 'bom_adm_js', plugins_url( 'assets/wc-bom.js', ), [ 'jquery' ] );
-		//wp_register_script( 'bom_adm_min_js', plugins_url( $url . 'wc-bom-admin.min.js', __FILE__ ), [ 'jquery' ] );
-		//wp_register_script( 'bom_adm_a_js', plugins_url( 'assets/wc-bom-admin.js', __FILE__ ), [ 'jquery' ] );
-		//wp_register_script( 'bom_adm_min_js', plugins_url( $url . 'wc-bom-admin.min.js', __FILE__ ), [ 'jquery' ] );
-
-		wp_enqueue_script( 'bom_adm_js' );
-		//wp_enqueue_script( 'bom_adm_a_js' );
-
-		wp_enqueue_script( 'sweetalertjs', 'https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js' );
-		wp_enqueue_style( 'sweetalert_css', 'https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css' );
-
-		wp_register_script( 'chosen_js', 'https://cdnjs.cloudflare.com/ajax/libs/chosen/1.7.0/chosen.jquery.min.js', [ 'jquery' ] );
-		wp_register_style( 'chosen_css', 'https://cdnjs.cloudflare.com/ajax/libs/chosen/1.7.0/chosen.min.css' );
-		//wp_enqueue_script( 'bom_adm_js' );
-		wp_enqueue_script( 'chosen_js' );
-		wp_enqueue_style( 'chosen_css' );
-		//wp_enqueue_style( 'bom_css' );
-	}
+    /**
+     * @var null
+     */
+    protected static $instance = null;
 
 
-	/**
-	 * @param $actions
-	 * @param $plugin_file
-	 *
-	 * @return array
-	 */
-	public function plugin_links( $actions, $plugin_file ) {
+    /**
+     * WCB_Init constructor.
+     */
+    private function __construct()
+    {
 
-		static $plugin;
+        $this->init();
+    }
 
-		if ( $plugin === null ) {
-			$plugin = plugin_basename( __FILE__ );
-		}
-		if ( $plugin === $plugin_file ) {
-			$settings = [
-				//edit.php?post_type=part
-				'parts'    => '<a href="edit.php?post_type=part">' . __( 'Parts', 'wc-bom' ) . '</a>',
-				'assembly' => '<a href="edit.php?post_type=assembly">' . __( 'Assembly', 'wc-bom' ) . '</a>',
+    /**
+     *
+     */
+    protected function init()
+    {
+        require __DIR__ . '/includes/class-wcb-post.php';
+        require __DIR__ . '/includes/class-wcb-groups.php';
+        require __DIR__ . '/includes/admin/class-wcb-settings-api.php';
+        require __DIR__ . '/includes/admin/class-wcb-settings.php';
 
-				'settings' => '<a href="admin.php?page=wc-bill-materials">' . __( 'Settings', 'wc-bom' ) . '</a>',
-			];
-			$actions  = array_merge( $settings, $actions );
-		}
+        $g = WCB_Field_Groups::getInstance();
+        $t = new WeDevs_Settings_API_Test();
+        $p = WCB_Post::getInstance();
 
-		return $actions;
-	}
+        add_action('init', [ $this, 'load_assets' ]);
+
+        //add_action( 'init', [ $this, 'check_acf' ] );
+        add_filter('plugin_action_links', [ $this, 'plugin_links' ], 10, 5);
+        register_activation_hook(__FILE__, [ $this, 'activate' ]);
+        register_deactivation_hook(__FILE__, [ $this, 'deactivate' ]);
+    }
+
+    /**
+     * @return null
+     */
+    public static function getInstance()
+    {
+
+        if (static::$instance === null) {
+            static::$instance = new static;
+        }
+
+        return static::$instance;
+    }
+
+    /**
+     *
+     */
+    public function activate()
+    {
+        flush_rewrite_rules();
+    }
+
+    /**
+     *
+     */
+    public function deactivate()
+    {
+        flush_rewrite_rules();
+    }
+
+    /**
+     *
+     */
+    public function load_assets()
+    {
+
+        wp_register_script('bom_adm_js', plugins_url('assets/wc-bom.js', __FILE__), [ 'jquery' ]);
+        //wp_register_script( 'bom_adm_min_js', plugins_url( $url . 'wc-bom-admin.min.js', __FILE__ ), [ 'jquery' ] );
+        //wp_register_script( 'bom_adm_a_js', plugins_url( 'assets/wc-bom-admin.js', __FILE__ ), [ 'jquery' ] );
+        //wp_register_script( 'bom_adm_min_js', plugins_url( $url . 'wc-bom-admin.min.js', __FILE__ ), [ 'jquery' ] );
+
+        wp_enqueue_script('bom_adm_js');
+        //wp_enqueue_script( 'bom_adm_a_js' );
+
+        wp_enqueue_script('sweetalertjs', 'https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js');
+        wp_enqueue_style('sweetalert_css', 'https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css');
+
+        wp_register_script('chosen_js', 'https://cdnjs.cloudflare.com/ajax/libs/chosen/1.7.0/chosen.jquery.min.js', [ 'jquery' ]);
+        wp_register_style('chosen_css', 'https://cdnjs.cloudflare.com/ajax/libs/chosen/1.7.0/chosen.min.css');
+        //wp_enqueue_script( 'bom_adm_js' );
+        wp_enqueue_script('chosen_js');
+        wp_enqueue_style('chosen_css');
+        //wp_enqueue_style( 'bom_css' );
+    }
+
+
+    /**
+     * @param $actions
+     * @param $plugin_file
+     *
+     * @return array
+     */
+    public function plugin_links($actions, $plugin_file)
+    {
+
+        static $plugin;
+
+        if ($plugin === null) {
+            $plugin = plugin_basename(__FILE__);
+        }
+        if ($plugin === $plugin_file) {
+            $settings = [
+                //edit.php?post_type=part
+                'parts'    => '<a href="edit.php?post_type=part">' . __('Parts', 'wc-bom') . '</a>',
+                'assembly' => '<a href="edit.php?post_type=assembly">' . __('Assembly', 'wc-bom') . '</a>',
+
+                'settings' => '<a href="admin.php?page=wc-bill-materials">' . __('Settings', 'wc-bom') . '</a>',
+            ];
+            $actions  = array_merge($settings, $actions);
+        }
+
+        return $actions;
+    }
 }
 
 
-if ( ! class_exists( 'WCB_Core' ) ) {
-	$wcb_core = WCB_Core::getInstance();
+if (! class_exists('WCB_Core')) {
+    $wcb_core = WCB_Core::getInstance();
 }
