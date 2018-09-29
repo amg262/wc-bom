eabf778b993a990307681ec15468e06b950f7dc4
Andrew Gunn <andrewmgunn26@gmail.com>
2018-09-28 22:38:45 -0500

    t

--- wc-bom.php
+++ wc-bom.php
@@ -27,6 +27,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( 'No direct access allowed' );
 }
 
+define( 'WC_BOM_TBL', 'wc_bom' );
+
+
+add_action( 'admin_notices', 'check_woocommerce' );
+
 function check_woocommerce() {
 
 	if ( class_exists( 'Woocommerce' ) ) {
@@ -35,18 +40,17 @@ function check_woocommerce() {
 	}
 }
 
-add_action( 'admin_notices', 'check_woocommerce' );
-//
-///* Checks to see if the acf pro plugin is activated  */
-//if ( ! is_plugin_active( 'advanced-custom-fields-pro/acf.php' ) ) {
-//  /* load the plugin and anything else you want to do */
-//}
-//
-///* Checks to see if the acf plugin is activated  */
-//if ( ! is_plugin_active( 'advanced-custom-fields/acf.php' ) ) {
-//
-//}
-/** Start: Detect ACF Pro plugin. Include if not present. */
+add_action( 'admin_init', 'check_acf' );
+
+function check_acf() {
+	$acf     = 'advanced-custom-fields/acf.php';
+	$active  = in_array( $acf, apply_filters( 'active_plugins', get_option( 'active_plugins' ) ), true );
+	$has_acf = plugin_dir_url( $acf );
+
+	echo $active;
+}
+
+
 if ( ! class_exists( 'acf' ) ) { // if ACF Pro plugin does not currently exist
 	/** Start: Customize ACF path */
 	add_filter( 'acf/settings/path', 'cysp_acf_settings_path' );
@@ -110,130 +114,75 @@ function cysp_acf_json_load_point( $paths ) {
 	return $paths;
 }
 
+/**
+ *
+ */
+function create_options() {
 
 
+}
 
-class WCB_Core {
+/**
+ *
+ */
+function install_data() {
+	global $wpdb;
 
-	/**
-	 * @var null
-	 */
-	protected static $instance = null;
+	$welcome_name = 'Mr. WordPress';
+	$welcome_text = 'Congratulations, you just completed the installation!';
 
+	$table_name = $wpdb->prefix . WCB_TBL;
 
-	/**
-	 * WCB_Init constructor.
-	 */
-	private function __construct() {
-
-		$this->init();
-	}
-
-	/**
-	 *
-	 */
-	protected function init() {
-
-		if ( ! add_option( 'wc_bom_1', '' ) ) {
-			update_option( 'wc_bom_1', 'true' );
-		}
-
-		$opt = get_option( 'wc_bom_1' );
-		//echo json_encode( $opt );
-		require __DIR__ . '/includes/class-wcb-post.php';
-		require __DIR__ . '/includes/class-wcb-groups.php';
-		require __DIR__ . '/includes/admin/class-wcb-settings-api.php';
-		require __DIR__ . '/includes/admin/class-wcb-settings.php';
-
-		$g = WCB_Field_Groups::getInstance();
-		$t = new WeDevs_Settings_API_Test();
-		//$p = WCB_Post::getInstance();
-
-		add_action( 'init', [ $this, 'load_assets' ] );
-
-		//add_action( 'init', [ $this, 'check_acf' ] );
-		add_filter( 'plugin_action_links', [ $this, 'plugin_links' ], 10, 5 );
-		register_activation_hook( __FILE__, [ $this, 'activate' ] );
-		register_deactivation_hook( __FILE__, [ $this, 'deactivate' ] );
-
-	}
-
-	/**
-	 * @return null
-	 */
-	public static function getInstance() {
-
-		if ( static::$instance === null ) {
-			static::$instance = new static;
-		}
-
-		return static::$instance;
-	}
-
-	/**
-	 *
-	 */
-	public function activate() {
-
-		flush_rewrite_rules();
-	}
-
-	/**
-	 *
-	 */
-	public function deactivate() {
-
-		flush_rewrite_rules();
-	}
-
-	/**
-	 *
-	 */
-	public function load_assets() {
-
-		wp_register_script( 'bom_adm_js', plugins_url( 'assets/wc-bom.js', __FILE__ ), [ 'jquery' ] );
-
-		wp_enqueue_script( 'bom_adm_js' );
-		//wp_enqueue_script( 'bom_adm_a_js' );
-
-		wp_enqueue_script( 'sweetalertjs', 'https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js' );
-		wp_enqueue_style( 'sweetalert_css', 'https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css' );
-
-		wp_register_script( 'chosen_js', 'https://cdnjs.cloudflare.com/ajax/libs/chosen/1.7.0/chosen.jquery.min.js', [ 'jquery' ] );
-		wp_register_style( 'chosen_css', 'https://cdnjs.cloudflare.com/ajax/libs/chosen/1.7.0/chosen.min.css' );
-		//wp_enqueue_script( 'bom_adm_js' );
-		wp_enqueue_script( 'chosen_js' );
-		wp_enqueue_style( 'chosen_css' );
-		//wp_enqueue_style( 'bom_css' );
-	}
-
-	/**
-	 * @param $actions
-	 * @param $plugin_file
-	 *
-	 * @return array
-	 */
-	public function plugin_links( $actions, $plugin_file ) {
-
-		static $plugin;
-
-		if ( $plugin === null ) {
-			$plugin = plugin_basename( __FILE__ );
-		}
-		if ( $plugin === $plugin_file ) {
-			$settings = [
-				//edit.php?post_type=part
-				'parts'    => '<a href="edit.php?post_type=part">' . __( 'Parts', 'wc-bom' ) . '</a>',
-				'assembly' => '<a href="edit.php?post_type=assembly">' . __( 'Assembly', 'wc-bom' ) . '</a>',
-
-				'settings' => '<a href="admin.php?page=wc-bill-materials">' . __( 'Settings', 'wc-bom' ) . '</a>',
-			];
-			$actions  = array_merge( $settings, $actions );
-		}
-
-		return $actions;
-	}
+	$wpdb->insert( $table_name, [
+		'time' => current_time( 'mysql' ),
+		'name' => $welcome_name,
+		'data' => $welcome_text,
+		'url'  => 'http://cloudground.net/',
+	] );
 }
 
 
-$wcb_core = WCB_Core::getInstance();
+/**
+ *
+ */
+function delete_db() {
+	global $wpdb;
+
+	$table_name = $wpdb->prefix . WCB_TBL;
+
+	//$q = "SELECT * FROM " . $table_name . " WHERE id > 0  ;";
+	$wpdb->query( "DROP TABLE IF EXISTS  . $table_name . " );
+}
+
+/**
+ *
+ */
+function upgrade_data() {
+	global $wpdb;
+
+	global $wcb_data;
+
+
+	$table_name = $wpdb->prefix . WCB_TBL;
+
+	$sql = "CREATE TABLE IF NOT EXISTS $table_name (
+					id int(11) NOT NULL AUTO_INCREMENT,
+					title varchar(255),
+					post_id int(11),
+					type varchar(255),
+					sub_ids text,
+					data text ,
+					time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+					active tinyint(1) DEFAULT -1 NOT NULL,
+					PRIMARY KEY  (id)
+				);";
+
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+
+	dbDelta( $sql );
+
+
+}
+
+require __DIR__ . '/core.php';
